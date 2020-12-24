ba1e2043fb17cdada43b705be6556844917841a6
kjmj <kmorton15@gmail.com>
2020-12-24 12:24:25 -0500

    add support for chrome browser user agent string

--- lib/user_agent_parser.dart
+++ lib/user_agent_parser.dart
@@ -1,7 +1,78 @@
 library user_agent_parser;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class Result {
+  Browser browser;
+  Result({
+    this.browser,
+  });
+}
+
+class Browser {
+  String name;
+  String version;
+
+  Browser({this.name, this.version});
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Browser &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          version == other.version;
+
+  @override
+  int get hashCode => name.hashCode ^ version.hashCode;
+}
+
+class UserAgentParser {
+  /// Parse a [Result] from the [userAgent] string.
+  ///
+  /// TODO: Add other types (engine, os, device, cpu) into [Result]
+  Result parseResult(String userAgent) {
+    return Result(
+      browser: parseBrowser(userAgent),
+    );
+  }
+
+  /// Parse a [Browser] from the [userAgent] string.
+  ///
+  /// Returns `null` if no match.
+  Browser parseBrowser(String userAgent) {
+    for (MapEntry mapEntry in _browsers.entries) {
+      String name = mapEntry.key;
+      List<String> regexes = mapEntry.value;
+
+      for (String regex in regexes) {
+        RegExp regExp = new RegExp(regex, caseSensitive: false);
+
+        if (regExp.hasMatch(userAgent)) {
+          final match = regExp.firstMatch(userAgent).group(0);
+
+          return new Browser(
+            name: name,
+            version: match.split('/')[1],
+          );
+        }
+      }
+    }
+
+    return null;
+  }
+
+  /// Identifies the regexes for each browser.
+  ///
+  /// Key is the browser name.
+  ///    - ex. 'Chrome'
+  /// Value is a list of regex strings such that when matched with a user agent string,
+  /// it will return a match of the format `name/version`
+  ///    - ex. 'Chrome/87.0.4280.88'
+  ///
+  ///  TODO: Add support for Firefox, IE, Konqueror, Netscape, Opera, and Safari
+  final _browsers = <String, List<String>>{
+    'Chrome': [
+      r"chrome\/v?([\w\.]+)", // Chrome
+      r"((?:android.+)crmo|crios)\/([\w\.]+)", // Chrome for Android/iOS/iPad
+    ],
+  };
 }
--- test/user_agent_parser_test.dart
+++ test/user_agent_parser_test.dart
@@ -3,11 +3,91 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:user_agent_parser/user_agent_parser.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  var parser;
+  setUp(() {
+    parser = UserAgentParser();
+  });
+
+  group('chrome browser on', () {
+    test('windows', () {
+      final userAgent =
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36';
+      final actual = parser.parseBrowser(userAgent);
+      final expected = new Browser(name: 'Chrome', version: '87.0.4280.88');
+
+      expect(actual, expected);
+    });
+
+    test('macOS', () {
+      final userAgent =
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36';
+      final actual = parser.parseBrowser(userAgent);
+      final expected = new Browser(name: 'Chrome', version: '87.0.4280.88');
+
+      expect(actual, expected);
+    });
+
+    test('linux', () {
+      final userAgent =
+          'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36';
+      final actual = parser.parseBrowser(userAgent);
+      final expected = new Browser(name: 'Chrome', version: '87.0.4280.88');
+
+      expect(actual, expected);
+    });
+
+    test('iPhone', () {
+      final userAgent =
+          'Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1';
+      final actual = parser.parseBrowser(userAgent);
+      final expected = new Browser(name: 'Chrome', version: '87.0.4280.77');
+
+      expect(actual, expected);
+    });
+
+    test('iPad', () {
+      final userAgent =
+          'Mozilla/5.0 (iPad; CPU OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1';
+      final actual = parser.parseBrowser(userAgent);
+      final expected = new Browser(name: 'Chrome', version: '87.0.4280.77');
+
+      expect(actual, expected);
+    });
+
+    test('iPod', () {
+      final userAgent =
+          'Mozilla/5.0 (iPod; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1';
+      final actual = parser.parseBrowser(userAgent);
+      final expected = new Browser(name: 'Chrome', version: '87.0.4280.77');
+
+      expect(actual, expected);
+    });
+
+    test('android standard', () {
+      final userAgent =
+          'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36';
+      final actual = parser.parseBrowser(userAgent);
+      final expected = new Browser(name: 'Chrome', version: '87.0.4280.101');
+
+      expect(actual, expected);
+    });
+
+    test('android samsung', () {
+      final userAgent =
+          'Mozilla/5.0 (Linux; Android 10; SM-A205U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36';
+      final actual = parser.parseBrowser(userAgent);
+      final expected = new Browser(name: 'Chrome', version: '87.0.4280.101');
+
+      expect(actual, expected);
+    });
+
+    test('android lg', () {
+      final userAgent =
+          'Mozilla/5.0 (Linux; Android 10; LM-Q720) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36';
+      final actual = parser.parseBrowser(userAgent);
+      final expected = new Browser(name: 'Chrome', version: '87.0.4280.101');
+
+      expect(actual, expected);
+    });
   });
 }
