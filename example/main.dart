94ebb9e03b83b7a1e28f799c34a772df7d305bf2
kjmj <kmorton15@gmail.com>
2020-12-28 20:41:31 -0500

    update example

--- example/main.dart
+++ example/main.dart
@@ -4,9 +4,12 @@ void main() {
   UserAgentParser parser = UserAgentParser();
 
   String userAgent1 =
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36';
+      'Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1';
   Result result = parser.parseResult(userAgent1);
 
   print(result.browser.name); // Chrome
-  print(result.browser.version); // 87.0.4280.88
+  print(result.browser.unformattedName); // CriOS
+  print(result.browser.version); // 87.0.4280.77
+  print(result.browser
+      .parsedWithRegex); // r'(?<unformattedName>crmo|crios)\/(?<version>[\w\.]+)'
 }
